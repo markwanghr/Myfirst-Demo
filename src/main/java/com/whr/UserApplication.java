f59b83bdeeedac1ace607b7a60d5a56beca98ccc
mark <15155254588@163.com>
2018-10-08 11:34:38 +0800

    redis缓存以及定时任务

--- src/main/java/com/whr/UserApplication.java
+++ src/main/java/com/whr/UserApplication.java
@@ -5,12 +5,15 @@ import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //扫描mybatis mapper包的路径
 @MapperScan(basePackages = "com.whr.mapper")
 //扫描所有的包，包括一些自用的工具类包所在的路径
-//@ComponentScan(basePackages = {"com.whr"})
+@ComponentScan(basePackages = {"com.whr"})
+//开启定时任务
+@EnableScheduling
 public class UserApplication {
 
 	public static void main(String[] args) {
